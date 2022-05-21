aae90ca35721fc36ca4cf29b6f338b013a6552c2
Sarvagya Verma <sarvagya1115@gmail.com>
2022-05-21 23:36:53 +0530

    Added search_in_2d_matrix.cpp

--- 2D Arrays/search_in_2d_matrix.cpp
+++ 2D Arrays/search_in_2d_matrix.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
 bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int r = matrix.size();
         int c = matrix[0].size();
@@ -20,12 +22,14 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
     }
 
 int main(){
-    vector<vector<int>>& matrix = {
-        {{1, 3, 5 ,7},
+    vector<vector<int>> matrix = {
+        {1, 3, 5 ,7},
         {10, 11, 16, 20},
-        {23, 30, 34, 60}};
+        {23, 30, 34, 60}
     };
-    int target = 34;
+    int target = 60;
+
+
     cout<<searchMatrix(matrix, target);
     return 0;
 }
