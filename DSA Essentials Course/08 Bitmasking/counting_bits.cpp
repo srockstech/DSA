9df817858ada38cbd9c79def944390c827293309
Sarvagya Verma <sarvagya1115@gmail.com>
2022-12-30 01:27:05 +0530

    Add notes for count_bits in counting_bits.cpp

--- DSA Essentials Course/08 Bitmasking/counting_bits.cpp
+++ DSA Essentials Course/08 Bitmasking/counting_bits.cpp
@@ -3,15 +3,16 @@ using namespace std;
 
 int count_bits(int n){
 	
-	int cnt = 0;
-
-	while(n>0){
-		int last_bit = (n&1);
-		cnt += last_bit;
-
-		n = n>>1;
-	}
-	return cnt;
+	//1111 - n (15)
+	//n&1 will be 1 if the 0th bit is 1 and it will be 0 if oth bit is 0.
+	//Then with every iteration, right shift 'n' by 1 and keep adding the result of n&1 to count.
+	//Run the loop until 'n' becomes zero. This will end the loop earlier since loop will terminate if no more '1's will be left in 'n'.
+	int count = 0;
+    while(n>0){
+        count += n&1;
+        n >>= 1;
+    }
+    return count;
 }
 
 //Counting Sets Bits : Hack  (Faster method)
@@ -36,7 +37,7 @@ int main(){
 	cin>>n;
 
 	cout<<count_bits(n) <<endl;
-	cout<<count_bits_hack(n) <<endl;
+	// cout<<count_bits_hack(n) <<endl;
 
 	return 0;
 }
