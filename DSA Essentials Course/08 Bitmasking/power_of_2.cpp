638bdca7b2bbf0a5f087e79c969eab746d4a62bc
Sarvagya Verma <sarvagya1115@gmail.com>
2022-12-30 00:49:39 +0530

    Add notes to power_of_2.cpp

--- DSA Essentials Course/08 Bitmasking/power_of_2.cpp
+++ DSA Essentials Course/08 Bitmasking/power_of_2.cpp
@@ -1,21 +1,19 @@
 #include<iostream>
 using namespace std;
 
-
-
+//Find if a given a number is power of 2 or not.
 int main(){
-
 	int n;
 	cin>>n;
 
+	//Property:
+	//If a numner 'n' is a power a power of 2, n & (n-1) = 0
+	//Ex: 16 = 10000 and 15 = 01111. 16 & 15 = 00000 = 0
 	if( (n & (n-1))==0){
 		cout<<"Power of 2";
 	}
 	else{
 		cout<<"Not a power of 2";
 	}
-
-
-
 	return 0;
 }
