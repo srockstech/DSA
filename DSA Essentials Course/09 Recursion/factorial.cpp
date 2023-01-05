74188be7e62196ac3e0a386ff162a10968a66408
Sarvagya Verma <sarvagya1115@gmail.com>
2023-01-05 23:57:54 +0530

    Added notes to factorial.cpp in Recursion

--- DSA Essentials Course/09 Recursion/factorial.cpp
+++ DSA Essentials Course/09 Recursion/factorial.cpp
@@ -1,14 +1,16 @@
 #include<iostream>
 using namespace std;
 
-
-
+//Find the foctorial of the given number.
+//TC: O(n), SC: O(n)
+//We just need to figure out two cases: Base case and recursive case.
+//Recursion takes more time due to the implicit stack it creates.
 int fact(int n){
 	//base case
 	if(n==0){
 		return 1;
 	}
-	//rec case
+	//recursive case
 	int ans = n*fact(n-1);
 	return ans;
 }
