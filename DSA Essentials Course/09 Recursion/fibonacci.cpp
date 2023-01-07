49edc85fec2d58868f7fc5de574ed701b38da9b8
Sarvagya Verma <sarvagya1115@gmail.com>
2023-01-07 23:58:44 +0530

    Added notes to fibonacci.cpp

--- DSA Essentials Course/09 Recursion/fibonacci.cpp
+++ DSA Essentials Course/09 Recursion/fibonacci.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 
 int fib(int n){
+	//base case
+	//Here there will be two base cases since if n = 1, we are not going to call fib(0) and fib(-1).
 	if(n==0 or n==1){
 		return n;
 	}
-	int f1 = fib(n-1);
-	int f2 = fib(n-2);
-	return f1 + f2;
+	//recursive case
+	return fib(n-1) + fib(n-2);
 }
 
 int main(){
 	int n;
 	cin>>n;
 	cout<<fib(n) <<endl;
-
 }
