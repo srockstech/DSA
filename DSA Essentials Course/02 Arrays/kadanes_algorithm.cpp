5f7f99cd8258dbc26bd8e0448e760ade6426b8a7
Sarvagya Verma <sarvagya1115@gmail.com>
2023-01-20 01:23:23 +0530

    Added notes to kadanes_algorithm.cpp

--- DSA Essentials Course/02 Arrays/kadanes_algorithm.cpp
+++ DSA Essentials Course/02 Arrays/kadanes_algorithm.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 using namespace std;
 
+//Find the mazimum subarray sum
+//Kadane's algorithm TC: O(n), SC: O(1)
+//We assume the largest sum will not be a negative number i.e, there must be a positive number in the given array.
+//This doesn't work when all the elements in the given array are negative.
+//While traversing the array keep adding the elements to a variable 'cs'.
+//If 'cs' becomes negative, make it zero i.e.discard the current subarray and start considering the subarray from the next element.
+
 int maximum_subarray_sum(int arr[],int n){
 
 	int cs = 0;
 	int largest = 0;
-
 	for(int i=0;i<n;i++){
 		cs = cs + arr[i];
 		if(cs < 0){
@@ -19,7 +25,7 @@ int maximum_subarray_sum(int arr[],int n){
 
 
 int main(){
-	//Array Containing 
+	
 	int arr[] = {-2,3,4,-1,5,-12,6,1,3};
 	int n = sizeof(arr)/sizeof(int);
 
