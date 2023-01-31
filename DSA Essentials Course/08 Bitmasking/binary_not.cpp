02fb9b9145a9966b559889a7b72c50ca4ee60870
Sarvagya Verma <sarvagya1115@gmail.com>
2023-01-31 19:39:31 +0530

    Added notes to binary_not.cpp

--- DSA Essentials Course/08 Bitmasking/binary_not.cpp
+++ DSA Essentials Course/08 Bitmasking/binary_not.cpp
@@ -1,18 +1,16 @@
 #include<iostream>
 using namespace std;
 
-
-
 int main(){
 
-	unsigned int x = 5;
-	int y = 5;
-	cout << (~x) <<endl;  //4294967290
-	cout << (~y);   //-6
+	unsigned int x = 5; //0000....28 Zeros....0101
+	int y = 5; //0000....28 Zeros....0101
+	cout << (~x) <<endl;  //1111....29 1s....010 = 4294967290
+	cout << (~y);   //1111....29 1s....010 = -6
 
-	//Difference is due to the fact that we take 2s complement to find the magnitude of a negative signed number.
+	//Difference is due to the fact that we take 2s complement to find the magnitude of a negative signed number.  
 	//And ~y gives 1111....29 1s....010 which will be considered negative since it is signed and has the most significant bit as 1.
-	//While ~x since it is unsigned, it is not negative and hence 1111....29 1s....010 will be directly converted to decimal giving 4294967290 as the answer.
+	//While ~x, since it is unsigned, is not negative and hence 1111....29 1s....010 will be directly converted to decimal giving 4294967290 as the answer.
 
 	return 0;
 }
